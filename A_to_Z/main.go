df99651e0e737e2622c08594b27c213d67a6d6cb
2thousandmax <2thousandmax@gmail.com>
2021-12-06 13:03:39 +0300

    -

---
+++ A_to_Z/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	// A encodes to Z, B to Y, ...
+	alphabet := []byte("abcdefghijklmnopqrstuvwxyz")
+	len := len(alphabet)
+	alphabetMap := make(map[string]byte)
+	for idx, val := range alphabet {
+		alphabetMap[string(val)] = alphabet[len-idx-1]
+	}
+
+	fmt.Println(alphabetMap)
+
+	cipher := strings.Split("yzhsufo_rh_nb_uze_wdziu", "_")
+	flag := func(s []string) { fmt.Printf("MetaCTF{%s}", strings.Join(s, "_")) }
+	result := make([]string, 0)
+
+	for i := range cipher {
+		word := []byte(cipher[i])
+		for i, char := range word {
+			word[i] = alphabetMap[string(char)]
+		}
+		result = append(result, string(word))
+	}
+
+	flag(result)
+}
